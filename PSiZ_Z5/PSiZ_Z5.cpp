166716790de9198ed1dcca7637671e545826db67
naDiel-D <altarro@o2.pl>
2022-06-25 21:46:33 +0100

    Add Details Function for Metadata

--- PSiZ_Z5/PSiZ_Z5.cpp
+++ PSiZ_Z5/PSiZ_Z5.cpp
@@ -44,3 +44,57 @@ struct coloursRGB
 } 
 Rgb;
 
+void Details(const char* file) // Metadane
+{
+    FILE* test = fopen(file, "rb");
+
+    if (test == nullptr)
+    {
+        cout << endl << "Blad otwarcia pliku!" << endl;
+    }
+    else
+    {
+        cout << endl << "Otwarcie pliku zakoczone pomyslnie!" << endl;
+
+        fread(&File.Type, sizeof(File.Type), 1, test);
+        fread(&File.Size, sizeof(File.Size), 1, test);
+        fread(&File.Reserved1, sizeof(File.Reserved1), 1, test);
+        fread(&File.Reserved2, sizeof(File.Reserved2), 1, test);
+        fread(&File.OffBits, sizeof(File.OffBits), 1, test);
+
+        fseek(test, 14, SEEK_SET);
+        fread(&Pic.headerSize, sizeof(Pic.headerSize), 1, test);
+        fread(&Pic.width, sizeof(Pic.width), 1, test);
+        fread(&Pic.height, sizeof(Pic.height), 1, test);
+        fread(&Pic.planes, sizeof(Pic.planes), 1, test);
+        fread(&Pic.bitPerPixel, sizeof(Pic.bitPerPixel), 1, test);
+        fread(&Pic.compresion, sizeof(Pic.compresion), 1, test);
+        fread(&Pic.imageSize, sizeof(Pic.imageSize), 1, test);
+        fread(&Pic.xPelsPerMeter, sizeof(Pic.xPelsPerMeter), 1, test);
+        fread(&Pic.yPelsPerMeter, sizeof(Pic.yPelsPerMeter), 1, test);
+        fread(&Pic.colorUsed, sizeof(Pic.colorUsed), 1, test);
+        fread(&Pic.colorImportant, sizeof(Pic.colorImportant), 1, test);
+
+        cout << endl << "INFO:" << endl << endl;
+
+        cout << "Typ pliku: " << File.Type << endl
+            << "Rozmiar pliku: " << File.Size << " bajtow" << endl
+            << "Zarezerwowane 1: " << File.Reserved1 << endl
+            << "Zarezerwowane 2: " << File.Reserved2 << endl
+            << "Pozycja danych obrazkowych: " << File.OffBits << endl << endl;
+
+        cout << "Wielkosc naglowka: " << Pic.headerSize << endl
+            << "Szerokosc obrazu: " << Pic.width << " pikseli" << endl
+            << "Wysokosc obrazu: " << Pic.height << " pikseli" << endl
+            << "Liczba platow: " << Pic.planes << endl
+            << "Liczba bitow na piksel: " << Pic.bitPerPixel << endl
+            << "Algorytm kompresji: " << Pic.compresion << endl
+            << "Rozmiar samego rysunku: " << Pic.imageSize << endl
+            << "Rozdzielczosc pozioma: " << Pic.xPelsPerMeter << endl
+            << "Rozdzielczosc pionowa: " << Pic.yPelsPerMeter << endl
+            << "Liczba kolorow w palecie: " << Pic.colorUsed << endl
+            << "Liczba waznych kolorow w palecie: " << Pic.colorImportant << endl;
+
+        fclose(test);
+    }
+}
